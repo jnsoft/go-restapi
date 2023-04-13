b006113be65ae046207998105fb003c3c0fb694f
Johan <jnsoft@users.noreply.github.com>
2023-04-14 00:56:48 +0200

    ssl added (#4)

--- mux-webservice/main.go
+++ mux-webservice/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"errors"
 	"go-restapi/mux-webservice/api"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	srv := api.NewServer()
-	http.ListenAndServe(":8080", srv)
+
+	wd, _ := os.Getwd()
+	cert_file := filepath.Join(wd, "/..", "localhost.crt")
+	key_file := filepath.Join(wd, "/..", "localhost.key")
+
+	use_https := true
+
+	if _, err := os.Stat(cert_file); errors.Is(err, os.ErrNotExist) {
+		use_https = false
+	}
+	if _, err := os.Stat(key_file); errors.Is(err, os.ErrNotExist) {
+		use_https = false
+	}
+
+	if use_https {
+		log.Fatal(http.ListenAndServeTLS(":9000", cert_file, key_file, srv))
+	} else {
+		http.ListenAndServe(":8080", srv)
+	}
+
 }
