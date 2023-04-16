112354a51ae1e97a73f8467bb33dee33587b5c2f
Johan <jnsoft@users.noreply.github.com>
2023-04-16 16:23:13 +0000

    db

--- gin-webservice/main.go
+++ gin-webservice/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
@@ -16,15 +20,34 @@ func main() {
 }
 
 func getAlbums(c *gin.Context) {
-	
-	c.IndentedJSON(http.StatusOK, albums)
+	urlExample := "postgres://postgres:123456@localhost:5432/albums"
+	// os.Getenv("DATABASE_URL")
+	conn, err := pgx.Connect(context.Background(), urlExample)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var title string
+	var artist string
+	var price float64
+	err = conn.QueryRow(context.Background(), "select title, artist, price from albums where id=$1", 1).Scan(&title, &artist, &price)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(title)
+
+	c.IndentedJSON(http.StatusOK, albums_static)
 }
 
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
 
 	// Loop over the list of albums, looking for an album whose ID value matches the parameter.
-	for _, a := range albums {
+	for _, a := range albums_static {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
@@ -42,11 +65,11 @@ func postAlbums(c *gin.Context) {
 	}
 
 	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
+	albums_static = append(albums_static, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
-var albums = []Album{
+var albums_static = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
